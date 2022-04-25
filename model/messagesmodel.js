0a5099c9cfdf5d73bea979eb24f5fba2f7f8d24f
sabinbaniya <dummymail@tutanota.de>
2022-04-25 09:07:17 +0545

    messagemodel add

--- model/messagesmodel.js
+++ model/messagesmodel.js
@@ -1,7 +1,21 @@
 const mongoose = require("mongoose");
 
-const MessageSchema = new mongoose.Schema({
-  author: {
-    type: String,
+const MessageSchema = new mongoose.Schema(
+  {
+    author: {
+      type: String,
+      required: true,
+    },
+    recieverChatroomId: {
+      type: String,
+      required: true,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const MessageModel = mongoose.model("messages", MessageSchema);
+
+module.exports = MessageModel;
